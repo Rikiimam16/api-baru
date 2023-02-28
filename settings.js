9bbcd4f8e69a777cfbd8aff97f3a00f382fc6ae7
Rikiimam16 <126227295+Rikiimam16@users.noreply.github.com>
2023-02-28 12:24:32 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
 
-global.creator = 'Zeeoneofc'// yourname
-global.MONGO_DB_URI = "" //database mongodb 
+global.creator = 'Riki'// yourname
+global.MONGO_DB_URI = "mongodb+srv://mrikiimamuddin16:tescVh2zdxSJbHa4@rizzz.ypuqs32.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "" //email
-global.email_password = "" //application password email
+global.your_email = "borutouzumaki77777777@gmail.com" //email
+global.email_password = "iyvssyqrmhaivbot" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
